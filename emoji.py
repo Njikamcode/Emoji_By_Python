8fb88d49a3b23bd52ce5b7dfd475ac34de9d0ff4
Njikamcode <33057880+Njikamcode@users.noreply.github.com>
2022-03-04 19:01:57 +0100

    Add files via upload

---
+++ emoji.py
@@ -0,0 +1,7 @@
+kissing = "\U0001F618"
+mask = "\U0001F637"
+happy = "\U0001F600"
+
+print(f"Hello my love {kissing}")
+print(f"Stay home and wear a mask {mask}")
+print(f"I'm very happy ! {happy}")
